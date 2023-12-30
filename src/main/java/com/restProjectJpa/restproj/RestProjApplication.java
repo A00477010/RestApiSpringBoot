818da210b369aeb90236e469b4eccc454b75ba0a
A00477010 <aditya.chaudhari@smu.ca>
2023-12-30 15:12:55 -0400

    Adds swagger

--- src/main/java/com/restProjectJpa/restproj/RestProjApplication.java
+++ src/main/java/com/restProjectJpa/restproj/RestProjApplication.java
@@ -2,12 +2,20 @@ package com.restProjectJpa.restproj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+//@EnableSwagger2
 @SpringBootApplication
 public class RestProjApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(RestProjApplication.class, args);
     }
+//    @Bean
+//    public webClient.Builder.getwebClient(){
+//        return webClient.builder();
+//    }
 
 }
